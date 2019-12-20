496a0085c38f428d9098003bac88935e8118e2d1
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2019-12-20 09:35:54 +0900

    edit to lowercase

--- sql/_32_damson/_01_regexp_functions/cases/regexp_replace_001.sql
+++ sql/_32_damson/_01_regexp_functions/cases/regexp_replace_001.sql
@@ -1,33 +1,33 @@
 -- test for regexp_replace with mandatory arguments
 
 --TEST: normal test
-SELECT REGEXP_REPLACE ('Database that helps you turn your next big idea into reality', '^(\S*)', 'Cubrid');
+SELECT regexp_replace ('Database that helps you turn your next big idea into reality', '^(\S*)', 'Cubrid');
 
 --TEST: pass no arguments
-SELECT REGEXP_REPLACE ();
+SELECT regexp_replace ();
 
 --TEST: pass wrong number of arguments
-SELECT REGEXP_REPLACE ('abc');
-SELECT REGEXP_REPLACE ('abc', '#');
+SELECT regexp_replace ('abc');
+SELECT regexp_replace ('abc', '#');
 
 --TEST: wrong regular expression pattern error
-SELECT REGEXP_REPLACE ('aabbb', 'ab{-1,3}$', '#');
-SELECT REGEXP_REPLACE ('aabbb', 'ab{3,2}$', '#');
+SELECT regexp_replace ('aabbb', 'ab{-1,3}$', '#');
+SELECT regexp_replace ('aabbb', 'ab{3,2}$', '#');
 
 --TEST: pass an empty string into arguments
-SELECT REGEXP_REPLACE ('', 'a|e|i|o|u', '#');
-SELECT REGEXP_REPLACE ('abc', '', '#');
-SELECT REGEXP_REPLACE ('abc', 'a|e|i|o|u', '');
-SELECT REGEXP_REPLACE ('', '', '#');
-SELECT REGEXP_REPLACE ('', 'a|e|i|o|u', '');
-SELECT REGEXP_REPLACE ('abc', '', '');
-SELECT REGEXP_REPLACE ('', '', '');
+SELECT regexp_replace ('', 'a|e|i|o|u', '#');
+SELECT regexp_replace ('abc', '', '#');
+SELECT regexp_replace ('abc', 'a|e|i|o|u', '');
+SELECT regexp_replace ('', '', '#');
+SELECT regexp_replace ('', 'a|e|i|o|u', '');
+SELECT regexp_replace ('abc', '', '');
+SELECT regexp_replace ('', '', '');
 
 --TEST: pass NULL into arguments
-SELECT REGEXP_REPLACE (NULL, 'a|e|i|o|u', '#'); -- src
-SELECT REGEXP_REPLACE ('abc', NULL, '#'); -- pattern
-SELECT REGEXP_REPLACE ('abc', 'a|e|i|o|u', NULL); -- replace
-SELECT REGEXP_REPLACE (NULL, NULL, '#'); -- src, pattern
-SELECT REGEXP_REPLACE (NULL, 'a|e|i|o|u', NULL); -- src, replace
-SELECT REGEXP_REPLACE ('abc', NULL, NULL); -- pattern, replace
-SELECT REGEXP_REPLACE (NULL, NULL, NULL); -- src, pattern, replace
+SELECT regexp_replace (NULL, 'a|e|i|o|u', '#'); -- src
+SELECT regexp_replace ('abc', NULL, '#'); -- pattern
+SELECT regexp_replace ('abc', 'a|e|i|o|u', NULL); -- replace
+SELECT regexp_replace (NULL, NULL, '#'); -- src, pattern
+SELECT regexp_replace (NULL, 'a|e|i|o|u', NULL); -- src, replace
+SELECT regexp_replace ('abc', NULL, NULL); -- pattern, replace
+SELECT regexp_replace (NULL, NULL, NULL); -- src, pattern, replace
--- sql/_32_damson/_01_regexp_functions/cases/regexp_replace_002.sql
+++ sql/_32_damson/_01_regexp_functions/cases/regexp_replace_002.sql
@@ -1,31 +1,31 @@
 -- test for regexp_replace with optional arguments
 
 --TEST: position argument
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 6);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 'a');
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 0);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', -1);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 999);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 6);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 'a');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 0);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', -1);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 999);
 
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', NULL);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', NULL, 2);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', NULL, 2, 'i');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', NULL);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', NULL, 2);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', NULL, 2, 'i');
 
 --TEST: occurrence argument
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 3);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 'a');
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, -1);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 999);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 3);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 'a');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, -1);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 999);
 
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, NULL);
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, NULL, 'i');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, NULL);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, NULL, 'i');
 
 --TEST: match_type argument
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'i');
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'c');
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, '');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'i');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'c');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, '');
 
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'a');
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'ia');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'a');
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, 'ia');
 
-SELECT REGEXP_REPLACE ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, NULL);
+SELECT regexp_replace ('a fox is cute', 'a|e|i|o|u', '#', 1, 0, NULL);
